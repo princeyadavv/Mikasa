4467f40ccc6e1b78eeb65905dc5ffebea3a46bd7
princeyadavv <85881452+princeyadavv@users.noreply.github.com>
2024-07-27 22:31:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 function App() {
-  const [todo, settodo] = useState("");
+  const [todo, settodo] = useState(""); 
   const [todos, settodos] = useState([]);
   const [showFinished, setshowFinished] = useState(true);
 
